b5c7901f55a79b063a99947fb526c5e549c95bd1
gabrielevanini <gabriele.vanini@gmail.com>
2025-03-04 17:48:21 +0100

    feat: add WIP movie detail refs #id-issue

--- src/app/pages/movie-detail/movie-detail.component.ts
+++ src/app/pages/movie-detail/movie-detail.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+interface IFormMovieDetail {
+  director: FormControl<any>;
+  title: FormControl<any>;
+}
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.scss'],
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnInit {
   constructor(private autService: AuthService) {}
+  formMovieDetail = new FormGroup<IFormMovieDetail>({
+    director: new FormControl<any>(null, {
+      validators: [Validators.required],
+    }),
+    title: new FormControl<any>(null, {
+      validators: [Validators.required],
+    }),
+  });
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
+  public saveMovie = () => {
+    return false;
+  };
 }
